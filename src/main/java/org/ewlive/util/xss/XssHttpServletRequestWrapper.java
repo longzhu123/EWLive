e906736ff4e35f4d1779181cb0677eb7aeeb4db9
yangjie <1174716220@qq.com>
2018-11-29 10:03:09 +0800

    XSSFilter添加

--- src/main/java/org/ewlive/util/xss/XssHttpServletRequestWrapper.java
+++ src/main/java/org/ewlive/util/xss/XssHttpServletRequestWrapper.java
@@ -4,33 +4,24 @@ package org.ewlive.util.xss;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
-
+/**
+ * Xss攻击ServletRequest转换
+ */
 public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
     public XssHttpServletRequestWrapper(HttpServletRequest servletRequest) {
-
         super(servletRequest);
-
     }
 
     public String[] getParameterValues(String parameter) {
-
         String[] values = super.getParameterValues(parameter);
-
         if (values == null) {
-
             return null;
-
         }
-
         int count = values.length;
-
         String[] encodedValues = new String[count];
-
         for (int i = 0; i < count; i++) {
-
             encodedValues[i] = cleanXSS(values[i]);
-
         }
 
         return encodedValues;
@@ -38,27 +29,17 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     }
 
     public String getParameter(String parameter) {
-
         String value = super.getParameter(parameter);
-
         if (value == null) {
-
             return null;
-
         }
-
         return cleanXSS(value);
-
     }
 
     public String getHeader(String name) {
-
         String value = super.getHeader(name);
-
         if (value == null)
-
             return null;
-
         return cleanXSS(value);
 
     }
