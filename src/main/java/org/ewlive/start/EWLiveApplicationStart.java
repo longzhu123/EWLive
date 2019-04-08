728718da513bc9a7aae22665464f437ea7faf488
yangjie <1174716220@qq.com>
2019-04-08 09:27:10 +0800

    去除多余的依赖

--- src/main/java/org/ewlive/start/EWLiveApplicationStart.java
+++ src/main/java/org/ewlive/start/EWLiveApplicationStart.java
@@ -2,7 +2,6 @@ package org.ewlive.start;
 
 
 import org.ewlive.listener.ApplicationStartedEventListener;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
